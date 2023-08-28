1880d404330b86e522a1e0567260ec700c62a635
htmlboss123 <91206261+htmlboss123@users.noreply.github.com>
2023-08-28 16:33:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = "21956488"
+API_HASH     = "812529f879f06436925c7d62eb49f5d1"
+BOT_TOKEN    = "6481049454:AAHpuh61OXk12sSG6E-hW_OMghxdypx6e6k"
 SESSION      = ""
-DATABASE_URI = ""
+DATABASE_URI = "mongodb+srv://FLITERBOT:@storefile.mqprfjm.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001884373226
 ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+CHANNEL      = "@vip_bro10"
