fe6b7288d2ea47e3790dc1ebf8ec37a305a927de
htmlboss123 <91206261+htmlboss123@users.noreply.github.com>
2023-08-28 17:47:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = "21956488"
 API_HASH     = "812529f879f06436925c7d62eb49f5d1"
 BOT_TOKEN    = "6481049454:AAHpuh61OXk12sSG6E-hW_OMghxdypx6e6k"
-SESSION      = ""
+SESSION      = "BQFPB4gAfwfbeM3IBRoZOEqosM2OWBUWE8ZZEBTb7gSesguBJozRY6wEdZdhPhhIsbXjflIdaVjXhpsy_tICyrCAAmxqT0tLRXfhMKI4CCeJP7bxTi6Vxo-_X5lHnADbhXhdVbo3zfVqBwHhbpMuCJBrOIzSrZ_Bx9E08Ekldg4fcEp3_uB-Ozjbfc_fP2POxXdBQRWVgAzy9PimWv436sc0KoScptWtnzVpwIa-wBAvnoneffsMhlqwVdCCLwmU-xeINIL2ZyuY5mgaP_K8AyfsAW_gCFs3iRfE8imNChBJmUBtCaf9K1ACYvudhvxpcQ0SNTyySlkPtjgMDC02jremq3-HFQAAAAB4ai0IAA"
 DATABASE_URI = "mongodb+srv://FLITERBOT:@storefile.mqprfjm.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
+LOG_CHANNEL  = -1001904265866
+ADMIN        = 2020224264
 CHANNEL      = "@vip_bro10"
